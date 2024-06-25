a50663a55d689491d9660ba011aeb7b54978728c
wonglok <yellowhappy831@gmail.com>
2024-06-25 19:16:49 +0800

    go

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  async rewrites() {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/dev/:path*/",
+          // destination: `${process.env.NEXT_PUBLIC_BASE_URL}/dev/:path*/`,
+          destination: `/api/hello`,
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
+  trailingSlash: true,
 };
 
+if (process.env.NODE_ENV === "development") {
+  async function Run() {
+    //
+    //
+  }
+  Run();
+}
 export default nextConfig;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter, Lobster } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
