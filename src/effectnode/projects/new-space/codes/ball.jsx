e7495b46bb4fd8e7bbae4e24ad034caefa95ea55
wonglok <yellowhappy831@gmail.com>
2024-06-27 11:13:15 +0800

    ball

--- src/effectnode/projects/new-space/codes/ball.jsx
+++ src/effectnode/projects/new-space/codes/ball.jsx
@@ -26,7 +26,7 @@ export function Runtime({ ui, io, useStore, onLoop }) {
     return onLoop(() => {
       let dt = clock.getDelta();
       if (ref.current) {
-        ref.current.rotation.y += dt * 5;
+        ref.current.rotation.y += dt * input.current["in0"];
       }
     });
   }, [onLoop]);
@@ -56,6 +56,7 @@ export function Runtime({ ui, io, useStore, onLoop }) {
         <PerspectiveCamera makeDefault position={[0, 0, 5]}></PerspectiveCamera>
       </Insert3D>
 
+      <div></div>
       {/* <span style={{ color: ui.baseColor }}>Runtime {ui.baseColor}</span> */}
     </>
   );
