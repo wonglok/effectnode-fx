d613322fe9ec3569cea36721a8e5c4e1b66800bb
wonglok <yellowhappy831@gmail.com>
2024-06-28 08:36:44 +0800

    save

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full select-none">
       <EffectNode projectName={json.title} useStore={useStore}></EffectNode>
     </div>
   );
