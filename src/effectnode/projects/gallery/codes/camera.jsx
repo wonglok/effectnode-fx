18f01a0a9ea04691304bac067b4cf08e13c4bcc8
wonglok <yellowhappy831@gmail.com>
2024-06-30 13:29:22 +0800

    save

---
+++ src/effectnode/projects/gallery/codes/camera.jsx
@@ -0,0 +1,20 @@
+import { PerspectiveCamera } from "@react-three/drei";
+
+export function ToolBox({ ui, useStore, domElement }) {
+  return <>camera</>;
+}
+
+export function Runtime({ ui, useStore, io }) {
+  let Insert3D = useStore((r) => r.Insert3D) || (() => null);
+
+  return (
+    <>
+      <Insert3D>
+        <PerspectiveCamera
+          makeDefault
+          position={[0, 0, ui.positionZ]}
+        ></PerspectiveCamera>
+      </Insert3D>
+    </>
+  );
+}
---
+++ src/effectnode/projects/gallery/codes/light.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+export function ToolBox({ ui, useStore, domElement }) {
+  return <>light</>;
+}
+
+export function Runtime({ ui, useStore, io }) {
+  let Insert3D = useStore((r) => r.Insert3D) || (() => null);
+
+  return (
+    <>
+      <Insert3D>
+        <pointLight
+          position={[-1.5, 0.5, 1]}
+          color={ui.pointLightColor}
+          intensity={ui.intensity}
+        ></pointLight>
+      </Insert3D>
+    </>
+  );
+}
+
+//
+
+//
+
+//
