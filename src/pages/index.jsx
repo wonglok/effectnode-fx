36816db54f8afd4b5989850053161e7134bf42d4
wonglok <yellowhappy831@gmail.com>
2024-06-30 09:01:40 +0800

    save

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
 import { useMemo } from "react";
 import { EffectNode } from "src/effectnode/runtime/EffectNode";
 import { create } from "zustand";
-import json from "../effectnode/projects/production-webgpu/graph.json";
+import json from "../effectnode/projects/production-webgl/graph.json";
+import gpu from "../effectnode/projects/examples-0001-webgpu-components/graph.json";
 export default function Home() {
-  let useStore = useMemo(() => {
+  let useStore1 = useMemo(() => {
     return create((set, get) => {
       return {
         //
@@ -13,9 +14,31 @@ export default function Home() {
     });
   }, []);
 
+  let useStore2 = useMemo(() => {
+    return create((set, get) => {
+      return {
+        //
+        settings: gpu.settings,
+        graph: gpu.graph,
+      };
+    });
+  }, []);
+
   return (
-    <div className="w-full h-full select-none no-sel-sub">
-      <EffectNode projectName={json.title} useStore={useStore}></EffectNode>
+    <div className="flex">
+      <div className=" w-80 h-80">
+        <div className="w-full h-full select-none no-sel-sub relative">
+          <EffectNode
+            projectName={json.title}
+            useStore={useStore1}
+          ></EffectNode>
+        </div>
+      </div>
+      <div className=" w-80 h-80">
+        <div className="w-full h-full select-none no-sel-sub relative">
+          <EffectNode projectName={gpu.title} useStore={useStore2}></EffectNode>
+        </div>
+      </div>
     </div>
   );
 }
