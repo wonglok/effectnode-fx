371af8345981c584244cd8fe8b584d9cf5ddc26e
wonglok <yellowhappy831@gmail.com>
2024-07-02 22:11:49 +0800

    auth

--- src/components/UIKit/Navbars/AuthNavbar.js
+++ src/components/UIKit/Navbars/AuthNavbar.js
@@ -34,7 +34,7 @@ export default function Navbar(props) {
                 className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap titlecase"
                 href="#pablo"
               >
-                App Prototype
+                Effect Node FX
               </a>
             </Link>
             <button
