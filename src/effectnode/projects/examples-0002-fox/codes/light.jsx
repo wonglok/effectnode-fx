aa629cd39e735165b049743a1b07c4808084f146
wonglok <yellowhappy831@gmail.com>
2024-07-10 15:04:42 +0800

    save

--- src/effectnode/projects/examples-0002-fox/codes/light.jsx
+++ src/effectnode/projects/examples-0002-fox/codes/light.jsx
@@ -1,9 +1,14 @@
 import { Environment, useTexture } from "@react-three/drei";
 import hdr from "../assets/hdr/symmetrical_garden_02_1k.hdr";
-import { EquirectangularReflectionMapping } from "three";
+import {
+  EquirectangularReflectionMapping,
+  FloatType,
+  HalfFloatType,
+} from "three";
 import { useLoader, useThree } from "@react-three/fiber";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import { Suspense, useEffect } from "react";
+import { texture, uv } from "three/examples/jsm/nodes/Nodes";
 export function ToolBox({}) {
   return <>toolbox</>;
 }
@@ -15,12 +20,12 @@ export function Runtime({ ui, useStore, io }) {
       <Insert3D>
         <Suspense fallback={null}>
           <Load></Load>
+          <pointLight
+            position={[0, 1.3, 5]}
+            color={ui.pointLightColor}
+            intensity={ui.intensity}
+          ></pointLight>
         </Suspense>
-        <pointLight
-          position={[0, 1.3, 5]}
-          color={ui.pointLightColor}
-          intensity={ui.intensity}
-        ></pointLight>
       </Insert3D>
     </>
   );
@@ -30,24 +35,27 @@ export function Runtime({ ui, useStore, io }) {
 
 //
 function Load() {
+  let gl = useThree((r) => r.gl);
   let scene = useThree((r) => r.scene);
   useEffect(() => {
     let rgbe = new RGBELoader();
-    scene.environment = null;
-    scene.background = null;
+    rgbe.setDataType(HalfFloatType);
 
-    rgbe.loadAsync(hdr).then((texture) => {
-      texture.mapping = EquirectangularReflectionMapping;
-      scene.environment = texture;
-      scene.background = texture;
+    rgbe.loadAsync(hdr).then((tex) => {
+      tex.mapping = EquirectangularReflectionMapping;
+
+      scene.backgroundNode = texture(tex, uv());
+      scene.environmentNode = texture(tex, uv());
     });
 
     return () => {};
-  }, [scene]);
+  }, [gl, scene]);
+
   return (
     <>
       {/*  */}
       {/* <Environment blur={0.15} background files={[hdr]}></Environment> */}
+      {/*  */}
     </>
   );
 }
--- src/effectnode/projects/examples-0002-fox/codes/main.jsx
+++ src/effectnode/projects/examples-0002-fox/codes/main.jsx
@@ -1,13 +1,16 @@
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useMemo, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useEffect, useMemo } from "react";
+//
+import WebGPUBackend from "three/examples/jsm/renderers/webgpu/WebGPUBackend";
 import WebGPURenderer from "three/examples/jsm/renderers/webgpu/WebGPURenderer";
 // import WebGPURenderer from "three/examples/jsm/renderers/webgpu/WebGPURenderer";
 import tunnel from "tunnel-rat";
+
 export function ToolBox({}) {
   return (
     <>
-      Note Yo
       {/*  */}
+      Note Yo
       {/*  */}
     </>
   );
@@ -16,70 +19,42 @@ export function ToolBox({}) {
 export function Runtime({ ui, useStore, io }) {
   let t3d = useMemo(() => tunnel(), []);
   //
-  let __activeCanvas = useStore((r) => r.__activeCanvas);
+
   useEffect(() => {
     useStore.setState({
       Insert3D: function Insert3D({ children }) {
         return <t3d.In>{children}</t3d.In>;
       },
-      __activeCanvas: true,
     });
 
-    return () => {
-      useStore.setState({
-        __activeCanvas: false,
-      });
-    };
+    return () => {};
   }, [useStore]);
-  //
 
   return (
     <>
-      {__activeCanvas && (
-        <Canvas gl={(canvas) => new WebGPURenderer({ canvas })}>
-          <RenderStuff useStore={useStore}>
-            <t3d.Out></t3d.Out>
-          </RenderStuff>
-        </Canvas>
-      )}
+      <Canvas
+        gl={(canvas) => {
+          let renderer = new WebGPURenderer({ canvas });
+          return renderer;
+        }}
+      >
+        <Render>{<t3d.Out></t3d.Out>}</Render>
+      </Canvas>
     </>
   );
 }
 
-function RenderStuff({ children, useStore }) {
+function Render({ children }) {
   //
-  let get = useThree((r) => r.get);
-  let ___canShow = useStore((r) => r.___canShow);
-  useEffect(() => {
-    setTimeout(() => {
-      try {
-        st.gl
-          .init()
-          .then(() => {
-            setTimeout(() => {
-              useStore.setState({ ___canShow: true });
-            });
-          })
-          .catch(() => {
-            setTimeout(() => {
-              useStore.setState({ ___canShow: true });
-            });
-          });
-      } catch (e) {
-        setTimeout(() => {
-          useStore.setState({ ___canShow: true });
-        });
-      }
-    });
-  }, [get, useStore]);
-
-  useFrame(({ gl, camera, scene }) => {
-    if (___canShow) {
-      gl.renderAsync(scene, camera);
-    }
+  useFrame(({ gl, scene, camera }) => {
+    gl.renderAsync(scene, camera);
   }, 100);
 
-  return <>{___canShow && children}</>;
+  return (
+    <>
+      {/*  */}
+      {children}
+      {/*  */}
+    </>
+  );
 }
-
-//
