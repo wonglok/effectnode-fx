5bde3ac1243cc3a3390ab5f7f4c65a5337d266c9
wonglok <yellowhappy831@gmail.com>
2024-07-24 21:49:00 +0800

    save

---
+++ src/effectnode/projects/webgpu-fluid-003-kernel/loklok/resolveCollisions.js
@@ -0,0 +1,64 @@
+import {
+  abs,
+  add,
+  float,
+  If,
+  max,
+  min,
+  sign,
+  vec3,
+} from "three/examples/jsm/nodes/Nodes";
+import { distanceTo } from "./distanceTo";
+
+export let resolveCollisions = ({
+  boundSizeMax,
+  boundSizeMin,
+  /** @type {vec3} */
+  position,
+  velocity,
+  collisionDamping = 1,
+  particleSize,
+  delta,
+}) => {
+  //
+
+  let nowPosition = vec3(position);
+
+  // let nOne = float(-5);
+  // let pOne = float(5);
+
+  If(nowPosition.x.greaterThanEqual(boundSizeMax.x), () => {
+    position.x.assign(boundSizeMax.x);
+    velocity.x.mulAssign(float(delta).mul(0.5));
+    velocity.x.addAssign(float(-1).mul(delta).mul(5));
+  });
+  If(nowPosition.x.lessThanEqual(boundSizeMin.x), () => {
+    position.x.assign(boundSizeMin.x);
+    velocity.x.mulAssign(float(delta).mul(0.5));
+    velocity.x.addAssign(float(1).mul(delta).mul(5));
+  });
+
+  If(nowPosition.y.greaterThanEqual(boundSizeMax.y), () => {
+    position.y.assign(boundSizeMax.y);
+    velocity.y.mulAssign(float(delta).mul(0.5));
+    velocity.y.addAssign(float(-1).mul(delta).mul(5));
+  });
+  If(nowPosition.y.lessThanEqual(boundSizeMin.y), () => {
+    position.y.assign(boundSizeMin.y);
+    velocity.y.mulAssign(float(delta).mul(0.5));
+    velocity.y.addAssign(float(1).mul(delta).mul(5));
+  });
+
+  If(nowPosition.z.greaterThanEqual(boundSizeMax.z), () => {
+    position.z.assign(boundSizeMax.z);
+    velocity.z.mulAssign(float(delta).mul(0.5));
+    velocity.z.addAssign(float(-1).mul(delta).mul(5));
+  });
+  If(nowPosition.z.lessThanEqual(boundSizeMin.z), () => {
+    position.z.assign(boundSizeMin.z);
+    velocity.z.mulAssign(float(delta).mul(0.5));
+    velocity.z.addAssign(float(1).mul(delta).mul(5));
+  });
+
+  //
+};
